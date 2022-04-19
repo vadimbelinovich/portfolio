70f11f667f6d86e41d526fb988c147b43b5e2c43
Vadim <vadimbelinovich@gmail.com>
2022-04-19 19:33:24 +0300

    first commit

---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BsGithub, BsInstagram } from "react-icons/bs";
+
+const SocialMedia = () => (
+	<div className='app__social'>
+		<div>
+			<BsGithub />
+		</div>
+		<div>
+			<BsInstagram />
+		</div>
+	</div>
+);
+
+export default SocialMedia;
